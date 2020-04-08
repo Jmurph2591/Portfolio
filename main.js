a7fb0017d9d720155d7001fe06fc7ad22e68aa80
Joe Murphy <jmurphy2591@gmail.com>
2020-04-08 19:53:15 -0400

    js file cleanup

--- main.js
+++ main.js
@@ -1,45 +1,4 @@
-
-/*
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || 
-    document.documentElement.scrollTop > 150) {
-    document.getElementById('nav').style.top = "0";
-  } else {
-    document.getElementById('nav').style.top = "-50px";
-  }
-
-}
-*/
-
-/*
-$(window).scroll(function(){
-    if (document.body.scrollTop > 20 || 
-        document.documentElement.scrollTop > 150) {
-        document.getElementById('nav').style.top = "0";
-      } else {
-        document.getElementById('nav').style.top = "-50px";
-      }
-});
-
-$(window).scroll(function() {
-    $('nav').fadeIn();
-});
-*/
-
-
-/*
-function showNav {
-  $(window).scroll(function(){
-    if (document.body.scrollTop > 20) {
-        document.getElementById('nav').style.cssText = "color: black; background-color: white";
-      } else (document.body.scrollTop < 20) {
-        document.getElementById('nav').style.cssText = "background-color: transparent";
-      }
-});
-}
-*/
+// Nav Bar Fade in/out
 
 function showNav {
   $(window).scroll(function(){
